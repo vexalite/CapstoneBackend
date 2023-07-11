23183f8a738a04ca7383372f7e9bb1b604f9c7d5
meme escape <memescap3@gmail.com>
2023-07-11 18:41:03 +0000

    project routes added

---
+++ controllers/project.ts
@@ -0,0 +1,70 @@
+import prisma from "../prisma/db"
+
+//get all
+export const getProjects = async(req,res) =>{
+ const project = await prisma.project.findMany({
+     where:{
+          counter: true
+     }
+ })
+ res.json({data: project,})
+}
+
+//get one
+
+export const getOneProject = async(req,res) =>{
+     const id = req.params.id
+
+     const project = await prisma.project.findFirst({
+          where:{
+               id,
+               // belongsToId: req.user.id
+
+          },
+     })
+     res.json({data: project})
+}
+
+
+
+export const createProject = async (req,res)=>{
+     const created = await prisma.project.create({
+         data: {
+             project_name: req.body.project_name,
+             timeframe: req.body.timeframe,
+             technology: req.body.technology,
+             description: req.body.description,
+             devlist: req.body.devlist,
+             businessId: req.company.id
+         },
+     })
+     res.json({data :created})
+}
+
+export const updateProject = async (req,res)=>{
+     const updated = await prisma.project.update({
+          where:{
+               id : req.params.id
+               // belongsToId: req.user.id
+          },
+          data:{
+            project_name: req.body.project_name,
+             timeframe: req.body.timeframe,
+             technology: req.body.technology,
+             description: req.body.description,
+             devlist: req.body.devlist,
+          }
+     })
+     res.json({data :updated})
+}
+
+
+export const deleteProject = async (req,res)=>{
+     const deleted = await prisma.project.delete({
+          where:{
+               id : req.params.id
+               // belongsToId: req.user.id
+          },
+     })
+     res.json({data :deleted})
+}
--- router/router.ts
+++ router/router.ts
@@ -3,27 +3,28 @@ import { body,validationResult } from 'express-validator'
 import { handleInputErrors } from '../utils/middleware'
 import { createCompany, deleteCompany, getCompanies, getOneCompany, updateCompany } from '../controllers/business'
 import { createDeveloper, deleteDeveloper, getDevelopers, getOneDeveloper, updateDeveloper } from '../controllers/developer'
+import { createProject, deleteProject, getOneProject, getProjects, updateProject } from '../controllers/project'
 
 
 
 
 const router = Router()
 
-//Products
-router.get('/project' , (req, res) => {
+//////////////////////////////////////               Project            //////////////////////////////////////
+router.get('/project' , getProjects, (req, res) => {
      res.json({message:"hey from nodemon"})
 })
-router.post('/project' , (req,res)=>{
+router.post('/project' , createProject,(req,res)=>{
      res.json({message:"project post success"})
      console.log(req.body)
 })
-router.get('/project/:id' , ()=>{})
-router.put('/project/:id' , body('name').isString(),handleInputErrors, (req,res)=>{
+router.get('/project/:id' ,getOneProject, ()=>{})
+router.put('/project/:id' , body('name').isString(),updateProject, (req,res)=>{
    
 })
-router.delete('/project/:id' , ()=>{})
+router.delete('/project/:id' ,deleteProject, ()=>{})
 
-//Update
+///////////////////////////////////////////          Update                //////////////////////////////////
 router.get('/company' ,getCompanies,(req,res)=>{
      res.json({message:"get done"})
 })
@@ -32,6 +33,8 @@ router.get('/company/:id' ,getOneCompany, ()=>{})
 router.put('/company/:id' ,updateCompany, ()=>{})
 router.delete('/company/:id' ,deleteCompany, ()=>{})
 
+
+///////////////////////////////////////         Developer Routes            ///////////////////////////
 router.get('/dev' ,getDevelopers, ()=>{})
 router.post('/dev' ,createDeveloper, ()=>{})
 router.get('/dev/:id' ,getOneDeveloper, ()=>{})
