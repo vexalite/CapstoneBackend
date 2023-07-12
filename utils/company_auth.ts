4352fa90f895af2e419a63f9da85db0cdf239c6b
meme escape <memescap3@gmail.com>
2023-07-12 10:08:24 +0000

    company auth issue solved

--- utils/company_auth.ts
+++ utils/company_auth.ts
@@ -1,52 +1,58 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-export const comparePasswords = (password, hash) => {
-  return bcrypt.compare(password, hash);
-};
+
+
+export const comparePasswords = (password, hash) =>{
+ return bcrypt.compare(password, hash)
+}
 
 export const hashPassword = (password) => {
-  return bcrypt.hash(password, 5);
-};
+     return bcrypt.hash(password, 6)
+}
 
-export const createJWT = (newcompany) => {
-  const token = jwt.sign(
-    {
-      id: newcompany.id,
-      username: newcompany.username,
-    },
-    process.env.JWT_SECRET_C
-  );
 
-  return token;
-};
+export const createJWT_C = (newcompany) =>{
+    //  console.log(newcompany.id, newcompany.username)
+     const token_c = jwt.sign({
+          id: newcompany.id,
+          // username: newcompany.username
+     },
+     process.env.JWT_SECRET)
+    //  console.log('Generated JWT:', token_c);
+    
+     return token_c
+}
 
-// Auth middleware
-export const comProtect = (req, res, next) => {
-  const bearer = req.headers.authorization;
+// console.log(createJWT_C)
+//auth middleware
+export const comProtect = (req, res, next)=>{
+const bearer = req.headers.authorization
+// console.log(req.headers.authorization)
 
-  if (!bearer) {
-    res.status(400);
-    res.json({ message: "Authorization header is missing" });
-    return;
-  }
+if(!bearer){
+     res.status(400)
+     res.send('get the fuck out bitch 😃')
+     return
+}
+const [, token_c] = bearer.split(' ')
 
-  const [, token] = bearer.split(' ');
+if (!token_c){
+     res.status(400)
+     res.send('unvalid token 😃')
+     return;
+}
 
-  if (!token) {
-    res.status(400);
-    res.json({ message: "token not found" });
-    return;
-  }
-
-  try {
-    const company = jwt.verify(token, process.env.JWT_SECRET_C);
-    req.company = company;
-    next();
-  } catch (e) {
-    console.error(e);
-    res.status(400);
-    res.json({ message: "Invalid token" });
-    return;
-  }
-};
+try{
+     const company = jwt.verify(token_c, process.env.JWT_SECRET)
+     req.company = company
+    //  console.log('try',com)
+     next()
+} catch (e){
+     // console.error(e)
+    //  console.log(token_c)
+     res.status(400)
+     res.send('unvalid token 😃')
+     return
+}
+}
