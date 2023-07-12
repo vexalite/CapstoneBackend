a135c9af8fd3dd8297c41a9dab4d71d00a291690
vexalite <125900539+vexalite@users.noreply.github.com>
2023-07-12 12:22:17 +0530

    Update middleware.ts

--- utils/middleware.ts
+++ utils/middleware.ts
@@ -1,13 +1,13 @@
 import { validationResult } from "express-validator"
-import { Request, Response, NextFunction } from 'express';
+// import { Request, Response, NextFunction } from 'express';
 
-export const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
+export const handleInputErrors = (req, res, next) => {
      const err = validationResult(req)
      console.log(err)
 
      if(!err.isEmpty()){
           res.status(400)
-          res.json({error:"no name"})
+          res.json({err})
      } else{
           next()
      }
