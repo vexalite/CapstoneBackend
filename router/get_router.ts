29eac12e4a7b7347d7cbd03beb82899caff533f9
vexalite <125900539+vexalite@users.noreply.github.com>
2023-07-12 14:55:05 +0530

    Update get_router.ts

--- router/get_router.ts
+++ router/get_router.ts
@@ -4,13 +4,13 @@ import { getOneProject } from '../controllers/project'
 import { getOneDeveloper } from '../controllers/developer'
 import { getOneCompany } from '../controllers/business'
 
-const getrouter = Router()
-getrouter.get('/project' , getProjects)
-getrouter.get('/dev' ,getDevelopers)
-getrouter.get('/company' ,getCompanies,(req,res)=>{
+const router = Router()
+router.get('/project' , getProjects)
+router.get('/dev' ,getDevelopers)
+router.get('/company' ,getCompanies,(req,res)=>{
      res.json({message:"get done"})
 })
-getrouter.get('/project/:id' ,getOneProject)
-getrouter.get('/dev/:id' ,getOneDeveloper)
-getrouter.get('/company/:id' ,getOneCompany)
-export default getrouter
+router.get('/project/:id' ,getOneProject)
+router.get('/dev/:id' ,getOneDeveloper)
+router.get('/company/:id' ,getOneCompany)
+export default router
