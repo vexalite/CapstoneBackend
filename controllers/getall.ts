8a462e8706aa21014923ad0f83ab99cab6279060
vexalite <125900539+vexalite@users.noreply.github.com>
2023-07-12 17:38:12 +0530

    Update getall.ts

--- controllers/getall.ts
+++ controllers/getall.ts
@@ -35,3 +35,100 @@ export const getCompanies = async (req, res) => {
      })
      res.json({data: project,})
     }
+
+
+
+
+
+
+
+/////////////////////////////////////////  SORTING   /////////////////////////////////////////
+
+
+   export const getAscCompanies = async (req, res) => {
+    try {
+      const companies = await prisma.business.findMany({
+        where: {
+          present: true // Include the related Present data
+        },orderBy: {
+          company_name: 'asc',
+        }
+      });
+  
+      res.json({ data: companies })
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+
+  export const getDescCompanies = async (req, res) => {
+    try {
+      const companies = await prisma.business.findMany({
+        where: {
+          present: true // Include the related Present data
+        },
+        orderBy: {
+          company_name: 'desc',
+        }
+      });
+  
+      res.json({ data: companies })
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+
+
+   
+
+    export const getAscDevelopers = async(req,res) =>{
+      const company = await prisma.developer.findMany({
+          where:{
+               counter:true
+          },
+          orderBy: {
+            dev_name: 'asc',
+          }
+      })
+      res.json({data: company,})
+
+     }
+     
+     export const getDescDevelopers = async(req,res) =>{
+      const company = await prisma.developer.findMany({
+          where:{
+               counter:true
+          },
+          orderBy: {
+            dev_name: 'desc',
+          }
+      })
+      res.json({data: company,})
+     }
+
+     export const getAscProjects = async(req,res) =>{
+      const project = await prisma.project.findMany({
+          where:{
+               counter: true
+          },
+          orderBy: {
+            project_name: 'desc',
+          }
+      })
+      res.json({data: project,})
+     }
+
+
+     export const getDescProjects = async(req,res) =>{
+      const project = await prisma.project.findMany({
+          where:{
+               counter: true
+          },
+          orderBy: {
+            project_name: 'desc',
+          }
+      })
+      res.json({data: project,})
+     }
